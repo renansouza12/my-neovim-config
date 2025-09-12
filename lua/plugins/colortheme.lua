eceef4e45e3bc7ce26b03fd8d4e5ba6b7e5a07bb
RenanSouza <renansouza0122@gmail.com>
2025-09-12 19:30:42 -0300

    color thee changed to poimandres

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,22 +1,53 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    lazy = false,
-    config = function()
-        require("catppuccin").setup({
-            flavour = "mocha", -- latte, frappe, macchiato, mocha
-            transparent_background = true,
-            integrations = {
-                neo_tree = true,
-                treesitter = true,
-                telescope = true,
-                cmp = true,
-                gitsigns = true,
-                lsp_trouble = true,
-                which_key = true,
-            },
-        })
-        vim.cmd.colorscheme("catppuccin")
-    end,
+  "olivercederborg/poimandres.nvim",
+  name = "poimandres",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("poimandres").setup {
+      bold_vert_split        = false, -- Set to true if you like bold window separators
+      dim_nc_background      = false, -- Dim background of non-current windows
+      disable_background     = false, -- Set true if you want transparency
+      disable_float_background = false,
+      disable_italics        = false, -- Disable all italics
+    }
+
+    -- enable colorscheme
+    vim.cmd.colorscheme("poimandres")
+
+    -- UI options for safety
+    vim.o.termguicolors = true
+    vim.o.background = "dark"
+
+    -- Integrations / highlights
+    -- Poimandres doesn’t ship explicit integrations like Catppuccin,
+    -- so we link highlights here to keep plugins consistent.
+    local hl = vim.api.nvim_set_hl
+    local colors = require("poimandres.palette")
+
+    -- Neo-tree background tweak
+    hl(0, "NeoTreeNormal", { bg = "none" })
+    hl(0, "NeoTreeNormalNC", { bg = "none" })
+
+    -- Telescope borders / prompt
+    hl(0, "TelescopeNormal", { bg = "none" })
+    hl(0, "TelescopeBorder", { fg = colors.blue, bg = "none" })
+    hl(0, "TelescopePromptNormal", { bg = "none" })
+    hl(0, "TelescopePromptBorder", { fg = colors.cyan, bg = "none" })
+
+    -- CMP menu
+    hl(0, "CmpNormal", { bg = "none" })
+
+    -- Gitsigns
+    hl(0, "GitSignsAdd", { fg = colors.green })
+    hl(0, "GitSignsChange", { fg = colors.yellow })
+    hl(0, "GitSignsDelete", { fg = colors.red })
+
+    -- Trouble (LSP diagnostics list)
+    hl(0, "TroubleNormal", { bg = "none" })
+
+    -- Which-key popup
+    hl(0, "WhichKeyFloat", { bg = "none" })
+  end,
 }
+
