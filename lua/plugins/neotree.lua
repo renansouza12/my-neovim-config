78b502dbdcf08e2eec536366d17534869c8aa73f
RenanSouza <renansouza0122@gmail.com>
2025-10-02 21:39:06 -0300

    wip: neotree plugin

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -10,19 +10,209 @@ return {
         },
         config = function()
             require("neo-tree").setup({
+                close_if_last_window = false,
+                popup_border_style = "rounded",
+                enable_git_status = true,
+                enable_diagnostics = true,
+                
+                -- Window settings
+                window = {
+                    position = "left",
+                    width = 30,
+                    mapping_options = {
+                        noremap = true,
+                        nowait = true,
+                    },
+                    mappings = {
+                        ["<space>"] = {
+                            "toggle_node",
+                            nowait = false,
+                        },
+                        ["<2-LeftMouse>"] = "open",
+                        ["<cr>"] = "open",
+                        ["<esc>"] = "cancel",
+                        ["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
+                        ["l"] = "focus_preview",
+                        ["S"] = "open_split",
+                        ["s"] = "open_vsplit",
+                        ["t"] = "open_tabnew",
+                        ["w"] = "open_with_window_picker",
+                        ["C"] = "close_node",
+                        ["z"] = "close_all_nodes",
+                        ["a"] = {
+                            "add",
+                            config = {
+                                show_path = "none"
+                            }
+                        },
+                        ["A"] = "add_directory",
+                        ["d"] = "delete",
+                        ["r"] = "rename",
+                        ["y"] = "copy_to_clipboard",
+                        ["x"] = "cut_to_clipboard",
+                        ["p"] = "paste_from_clipboard",
+                        ["c"] = "copy",
+                        ["m"] = "move",
+                        ["q"] = "close_window",
+                        ["R"] = "refresh",
+                        ["?"] = "show_help",
+                        ["<"] = "prev_source",
+                        [">"] = "next_source",
+                        ["i"] = "show_file_details",
+                    },
+                },
+                
+                -- Component configs (icons and styling)
+                default_component_configs = {
+                    container = {
+                        enable_character_fade = true
+                    },
+                    indent = {
+                        indent_size = 2,
+                        padding = 1,
+                        with_markers = true,
+                        indent_marker = "│",
+                        last_indent_marker = "└",
+                        highlight = "NeoTreeIndentMarker",
+                        with_expanders = nil,
+                        expander_collapsed = "",
+                        expander_expanded = "",
+                        expander_highlight = "NeoTreeExpander",
+                    },
+                    icon = {
+                        folder_closed = "",
+                        folder_open = "",
+                        folder_empty = "󰜌",
+                        default = "*",
+                        highlight = "NeoTreeFileIcon"
+                    },
+                    modified = {
+                        symbol = "[+]",
+                        highlight = "NeoTreeModified",
+                    },
+                    name = {
+                        trailing_slash = false,
+                        use_git_status_colors = true,
+                        highlight = "NeoTreeFileName",
+                    },
+                    git_status = {
+                        symbols = {
+                            added     = "✚",
+                            modified  = "",
+                            deleted   = "✖",
+                            renamed   = "󰁕",
+                            untracked = "",
+                            ignored   = "",
+                            unstaged  = "󰄱",
+                            staged    = "",
+                            conflict  = "",
+                        }
+                    },
+                },
+                
+                -- Filesystem settings
                 filesystem = {
                     filtered_items = {
+                        visible = false,
                         hide_dotfiles = false,
                         hide_gitignored = true,
+                        hide_hidden = true,
+                        hide_by_name = {
+                            "node_modules"
+                        },
+                        hide_by_pattern = {
+                            "*.meta",
+                            "*/src/*/tsconfig.json",
+                        },
+                        always_show = {
+                            ".gitignored",
+                        },
+                        never_show = {
+                            ".DS_Store",
+                            "thumbs.db"
+                        },
+                        never_show_by_pattern = {
+                            ".null-ls_*",
+                        },
                     },
+                    follow_current_file = {
+                        enabled = true,
+                        leave_dirs_open = false,
+                    },
+                    group_empty_dirs = false,
+                    hijack_netrw_behavior = "open_default",
+                    use_libuv_file_watcher = false,
+                    window = {
+                        mappings = {
+                            ["<bs>"] = "navigate_up",
+                            ["."] = "set_root",
+                            ["H"] = "toggle_hidden",
+                            ["/"] = "fuzzy_finder",
+                            ["D"] = "fuzzy_finder_directory",
+                            ["#"] = "fuzzy_sorter",
+                            ["f"] = "filter_on_submit",
+                            ["<c-x>"] = "clear_filter",
+                            ["[g"] = "prev_git_modified",
+                            ["]g"] = "next_git_modified",
+                            ["o"] = { "show_help", nowait=false, config = { title = "Order by", prefix_key = "o" }},
+                            ["oc"] = { "order_by_created", nowait = false },
+                            ["od"] = { "order_by_diagnostics", nowait = false },
+                            ["og"] = { "order_by_git_status", nowait = false },
+                            ["om"] = { "order_by_modified", nowait = false },
+                            ["on"] = { "order_by_name", nowait = false },
+                            ["os"] = { "order_by_size", nowait = false },
+                            ["ot"] = { "order_by_type", nowait = false },
+                        },
+                        fuzzy_finder_mappings = {
+                            ["<down>"] = "move_cursor_down",
+                            ["<C-n>"] = "move_cursor_down",
+                            ["<up>"] = "move_cursor_up",
+                            ["<C-p>"] = "move_cursor_up",
+                        },
+                    },
+                },
+                
+                -- Buffers settings
+                buffers = {
+                    follow_current_file = {
+                        enabled = true,
+                        leave_dirs_open = false,
+                    },
+                    group_empty_dirs = true,
+                    show_unloaded = true,
+                },
+                
+                -- Git status settings
+                git_status = {
+                    window = {
+                        position = "float",
+                        mappings = {
+                            ["A"]  = "git_add_all",
+                            ["gu"] = "git_unstage_file",
+                            ["ga"] = "git_add_file",
+                            ["gr"] = "git_revert_file",
+                            ["gc"] = "git_commit",
+                            ["gp"] = "git_push",
+                            ["gg"] = "git_commit_and_push",
+                        }
+                    }
                 },
             })
 
+            -- Keymaps
             vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle Neo-tree" })
             vim.keymap.set("n", "<leader>o", ":Neotree focus<CR>", { desc = "Focus Neo-tree" })
+            
+            -- Optional: Custom highlights (uncomment and adjust colors to your liking)
+            -- vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "#1e1e2e" })
+            -- vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "#1e1e2e" })
+            -- vim.api.nvim_set_hl(0, "NeoTreeDirectoryIcon", { fg = "#89b4fa" })
+            -- vim.api.nvim_set_hl(0, "NeoTreeGitAdded", { fg = "#a6e3a1" })
+            -- vim.api.nvim_set_hl(0, "NeoTreeGitModified", { fg = "#f9e2af" })
+            -- vim.api.nvim_set_hl(0, "NeoTreeGitDeleted", { fg = "#f38ba8" })
         end,
     },
-
+    
     -- nvim-cmp
     {
         "hrsh7th/nvim-cmp",
@@ -36,7 +226,6 @@ return {
         },
         config = function()
             local cmp = require("cmp")
-
             cmp.setup({
                 snippet = {
                     expand = function(args)
@@ -58,4 +247,3 @@ return {
         end,
     },
 }
-
