320f7b13a9cc54fb15fbf5faecf1dabadce3d1b0
renan <renansouza0122@gmail.com>
2026-04-15 00:52:41 -0400

    Change terminal toggle to Ctrl+T

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -7,13 +7,15 @@ return {
       direction = "float",
       start_in_insert = true,
       close_on_exit = true,
+      open_mapping = [[<c-t>]],
     }
 
     -- Normal mode toggle
-    vim.keymap.set("n", "<leader>t", "<cmd>ToggleTerm<CR>", { desc = "Toggle terminal" })
+    vim.keymap.set("n", "<C-t>", "<cmd>ToggleTerm<CR>", { desc = "Toggle terminal" })
 
-    -- Terminal mode: press <Esc> to close
+    -- Terminal mode: press <Esc> or <C-t> to close
     vim.keymap.set("t", "<Esc>", [[<C-\><C-n><cmd>ToggleTerm<CR>]], { desc = "Close terminal" })
+    vim.keymap.set("t", "<C-t>", [[<C-\><C-n><cmd>ToggleTerm<CR>]], { desc = "Close terminal" })
   end,
 }
 
